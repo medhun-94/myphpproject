822ed9dd44a95818b4ddd4561ea6c938e8fe2b9a
medhun-94 <83075434+medhun-94@users.noreply.github.com>
2021-05-06 10:02:20 +0530

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,3 @@
+ <?php echo '<p>Hello Folks, it is possible!!</p>'; ?> 
+ <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
+ <?php echo '<p>We have automated Docker image creation and deployment!!!</p>'; ?> 
