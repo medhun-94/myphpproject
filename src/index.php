ee999da4c0c46f3cb35de977196f856614074a9d
medhun-94 <83075434+medhun-94@users.noreply.github.com>
2021-05-06 10:28:29 +0530

    updated the index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,3 @@
- <?php echo '<p>Hello friends, it is possible!!</p>'; ?> 
+ <?php echo '<p>Hello guys, it is possible!!</p>'; ?> 
  <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
  <?php echo '<p>We have automated Docker image creation and deployment!!!</p>'; ?> 
