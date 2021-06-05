28cccefda3cc362e794aa402b11a8d6d48607ef2
medhun-94 <83075434+medhun-94@users.noreply.github.com>
2021-06-05 20:02:05 +0530

    added into enemy

--- src/index.php
+++ src/index.php
@@ -1,3 +1,3 @@
- <?php echo '<p>Hello friends, it is possible!!</p>'; ?> 
+ <?php echo '<p>Hello enemy, it is possible!!</p>'; ?> 
  <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
  <?php echo '<p>We have automated Docker image creation and deployment!!!</p>'; ?> 
